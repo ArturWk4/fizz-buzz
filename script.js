660079a0ebd3b15bfe0ba2501503b8a3259f5881
ArturWk4 <elkin.as@mail.ru>
2018-07-02 00:03:35 +0300

    beautiful implementation of task fizz-buzz

---
+++ script.js
@@ -0,0 +1,14 @@
+function fizbuzz(variable) {
+    let result = '';
+    if(variable % 3 === 0){
+        result = 'Fizz';
+    }
+    if(variable % 5 == 0) {
+        result += 'Buzz';
+    }
+    return result;
+}
+
+console.log('9: ' + fizbuzz(9));
+console.log('10: ' + fizbuzz(10));
+console.log('15: ' + fizbuzz(15));
